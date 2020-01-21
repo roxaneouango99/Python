08162d89b843e56f8a5e08fbe6d88df81397b5de
roxaneouango99 <roxaneouango99@gmail.com>
2020-01-21 12:13:07 -0500

    assignment#2

--- assignment#2/4#.py
+++ assignment#2/4#.py
@@ -7,4 +7,4 @@ for n in range(num):
         list.append(numbers)
 
 print("Max odd num in the list is :", max(list), "\nMin odd num in the list is :", min(list))
-print("the sum of odd num in the list and min num in the list is:", sum(list))
+print("the sum of odd num in the list and min num in the list is:", max(list)+min(list))
